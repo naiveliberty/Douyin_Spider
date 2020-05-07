8cc843152b2ad00eae05833f71f858987a1057a9
naiveliberty <fthemuse@foxmail.com>
2020-05-07 19:04:24 +0800

    express web server file

---
+++ get_signa.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var get_keys = require('./douyin_signature')
+
+// 获取抖音 _signature 参数
+app.get('/douyin', function (req, res) {
+    var uid = req.query["user_id"].toString();
+    var tac = req.query["tac"].toString();
+    keys = get_keys.get_signature(uid, tac);
+    if (keys) {
+        res.send(keys);
+        console.log(keys.toString());
+    } else {
+        res.send('keys未生成!');
+    }
+});
